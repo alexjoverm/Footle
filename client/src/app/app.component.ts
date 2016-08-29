420b705a07ca798b133c777bfed9861213d3fa67
Alex J <alexjovermorales@gmail.com>
2016-08-29 16:10:37 +0200

    Add sidenav
    Installed angular material 2 and configured the sidenav
    
    #TG-28

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Observable } from 'rxjs/Observable'
+import { MediumScreen } from 'app/_shared/constants'
 
 import { ApiService } from 'services/api.service'
 
@@ -9,10 +11,30 @@ import 'styles/app.scss'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  url = 'https://github.com/preboot/angular2-webpack'
+export class AppComponent implements OnInit {
+  isBigScreen = false
+  _sidenavOpened = false
 
   constructor(private api: ApiService) {
     // Do something with api
+    Observable.fromEvent(window, 'resize')
+      .map(() => ({
+        width: window.innerWidth,
+        height: window.innerHeight
+      }))
+      .subscribe(size => {
+        this.isBigScreen = window.innerWidth > MediumScreen
+      })
   }
+
+  ngOnInit() {
+    this.isBigScreen = window.innerWidth > MediumScreen
+    this._sidenavOpened = this.isBigScreen
+  }
+
+  set sidenavOpened(value) {
+    console.log(value)
+    this._sidenavOpened = value
+  }
+
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule }       from '@angular/core'
+import { NgModule, ApplicationRef }       from '@angular/core'
 import { BrowserModule }  from '@angular/platform-browser'
 import { HttpModule }     from '@angular/http'
 import { FormsModule }    from '@angular/forms'
 
+import { removeNgStyles, createNewHosts } from '@angularclass/hmr'
+
 import { MdCardModule }    from '@angular2-material/card'
 import { MdButtonModule }  from '@angular2-material/button'
 import { MdSidenavModule } from '@angular2-material/sidenav'
+import { MdToolbarModule } from '@angular2-material/toolbar'
 
 import { AppComponent }   from './app.component'
 import { HomeComponent }  from './home/home.component'
@@ -19,6 +22,7 @@ import { routing }        from './app.routing'
     HttpModule,
     FormsModule,
 
+    MdToolbarModule,
     MdCardModule,
     MdButtonModule,
     MdSidenavModule,
@@ -35,4 +39,21 @@ import { routing }        from './app.routing'
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(public appRef: ApplicationRef) {}
+  hmrOnInit(store) {
+    console.log('HMR store', store);
+  }
+  hmrOnDestroy(store) {
+    let cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
+    // recreate elements
+    store.disposeOldHosts = createNewHosts(cmpLocation);
+    // remove styles
+    removeNgStyles();
+  }
+  hmrAfterDestroy(store) {
+    // display new elements
+    store.disposeOldHosts();
+    delete store.disposeOldHosts;
+  }
+}
