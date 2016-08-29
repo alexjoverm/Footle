dfb1ca033f3c1fa4aa6f1e302c14fd86d8efc107
Alex J <alexjovermorales@gmail.com>
2016-08-29 16:50:12 +0200

    Merge with preboot

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Observable } from 'rxjs/Observable'
+import { MediumScreen } from 'app/_shared/constants'
 
-import { ApiService } from './shared';
+import { ApiService } from 'services/api.service'
 
-import '../style/app.scss';
+import 'styles/app.scss'
 
 @Component({
-  selector: 'my-app', // <my-app></my-app>
+  selector: 'ft-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  url = 'https://github.com/preboot/angular2-webpack';
+export class AppComponent implements OnInit {
+  isBigScreen = false
+  _sidenavOpened = false
 
   constructor(private api: ApiService) {
     // Do something with api
+    Observable.fromEvent(window, 'resize')
+      .map(() => ({
+        width: window.innerWidth,
+        height: window.innerHeight
+      }))
+      .subscribe(size => {
+        this.isBigScreen = window.innerWidth > MediumScreen
+      })
   }
+
+  ngOnInit() {
+    this.isBigScreen = window.innerWidth > MediumScreen
+    this._sidenavOpened = this.isBigScreen
+  }
+
+  set sidenavOpened(value) {
+    console.log(value)
+    this._sidenavOpened = value
+  }
+
 }
