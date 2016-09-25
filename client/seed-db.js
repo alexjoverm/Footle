56156037a312276cf8b69be0409d90f6738e9625
Alex J <alexjovermorales@gmail.com>
2016-09-25 15:05:18 +0200

    Generate fake initial data
    closes #1

--- client/seed-db.js
+++ client/seed-db.js
@@ -1,14 +1,25 @@
+const jsf = require('json-schema-faker')
+
+const users = require('./faker/user.json')
+const appointments = require('./faker/appointment.json')
+
 module.exports = () => {
   const data = {
-    users: []
+    admins: [],
+    employees: [],
+    customers: [],
+    appointments: []
   }
 
-  // Create 1000 users
-  for (let i = 0; i < 100; i++) {
-    data.users.push({
-      id: i,
-      name: `User${i}`
-    })
+  // Create 20 users
+  for (let i = 0; i < 10; i++) {
+    data.admins.push(Object.assign(jsf(users), { id: i, role: 'admin' }))
+    data.employees.push(Object.assign(jsf(users), { id: i, role: 'employee' }))
+    data.customers.push(Object.assign(jsf(users), { id: i, role: 'customer' }))
+  }
+
+  for (let i = 0; i < 30; i++) {
+    data.appointments.push(Object.assign(jsf(appointments), { id: i }))
   }
 
   return data
